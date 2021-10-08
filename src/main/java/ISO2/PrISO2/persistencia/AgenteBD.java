81fd00930f8261665173063068417be89525a5c2
jehero <jesusbasketalas@gmail.com>
2021-10-08 20:17:42 +0200

    BBDD 1.0

--- src/main/java/ISO2/PrISO2/persistencia/AgenteBD.java
+++ src/main/java/ISO2/PrISO2/persistencia/AgenteBD.java
@@ -22,27 +22,46 @@ public class AgenteBD {
 		Class.forName(driver);
 		Connection conn = DriverManager.getConnection(url);
 		conn.createStatement().execute(
-				"create table entrega(id int, tipoVacuna varchar(30), region varchar(30), grupoPrioridad varchar(30), lote varchar(45), fecha date, cantidad int, primary key(id))");
+				"create table entrega(fecha date, cantidad int" //Clave foranea sin primaria??
+				+ "constraint fecha foreign key references lote(fecha))");
 		conn.createStatement().execute(
-				"create table lote(id int, tipoVacuna varchar(30), entregas varchar(30), fecha date, cantidad int, farmaceutica varchar(30), primary key(id))");
+				"create table lote(id int, fecha date, cantidad int, farmaceutica varchar(30), primary key(id))");
 		conn.createStatement().execute(
-				"create table vacunacion(id int, tipoVacuna varchar(30), paciente varchar(30), fecha date, isSegundaDosis boolean, primary key(id))");
+				"create table tipoVacuna(nombre varchar(30), farmaceutica varchar(30), fechaAprobacion varchar(30), primary key(nombre), "
+				+ "constraint farmaceutica foreign key references lote(farmaceutica))");
+		conn.createStatement().execute(
+				"create table vacunacion(fecha date, isSegundaDosis int, nombre_tipovacuna varchar(30), dni_paciente varchar(30), "
+				+ "constraint nombre_tipovacuna foreign key references tipoVacuna(nombre)"
+				+ "constraint dni_paciente foreign key references paciente(dni))"); //Lío
+		conn.createStatement().execute(
+				"create table paciente(dni varchar(30), nombre varchar(30), apellidos varchar(30), primary key(dni))");
+		
 		
 		conn.createStatement().execute("insert into entrega values "
-				+ "(123456, 'ARM', 'Madrid', 'Ancianos', 'ADZG145', 2021-05-23, 5),"
-				+ "(435667, 'ARM', 'Cataluña', 'Jovenes', 'ADRT345', 2021-07-03, 500)"
-				+ "(134563, 'ARM', 'Andalucia', 'Niños', 'HBV123345', 2021-09-15, 300)"
-				+ "(457687, 'ARM', 'Murcia', 'Adultos', 'HBV128745', 2021-08-23, 3000)");
+				+ "(2021-05-23, 5),"
+				+ "(2021-07-03, 500)"
+				+ "(2021-09-15, 300)"
+				+ "(2021-08-23, 3000)");
 		conn.createStatement().execute("insert into lote values "
-				+ "(153456, 'ARM', '123456', 2021-05-24, 5, 'Astrazeneca'),"
-				+ "(485667, 'ARM', '123456', 2021-07-04, 500, 'Pfizer')"
-				+ "(194563, 'ARM', '435667', 2021-09-16, 300, 'Pfizer')"
-				+ "(477687, 'ARM', '457687', 2021-08-24, 3000, 'Moderna')");
+				+ "(153456, 2021-05-24, 5, 'Astrazeneca'),"
+				+ "(485667, 2021-07-04, 500, 'Pfizer')"
+				+ "(194563, 2021-09-16, 300, 'Pfizer')"
+				+ "(477687, 2021-08-24, 3000, 'Moderna')");
 		conn.createStatement().execute("insert into vacunacion values "
-				+ "(223456, 'ARM', 'Agustin', 'Ancianos', 'ADZG145', 2021-05-23, 5),"
-				+ "(835667, 'ARM', 'David', 'Jovenes', 'ADRT345', 2021-07-03, 500)"
-				+ "(934563, 'ARM', 'Andalucia', 'Niños', 'HBV123345', 2021-09-15, 300)"
-				+ "(757687, 'ARM', 'Murcia', 'Adultos', 'HBV128745', 2021-08-23, 3000)");
+				+ "(2021-05-26, 0, 'ARM', 223456)"
+				+ "(2021-04-24, 1, 'ARM', 123656)"
+				+ "(2021-02-23, 1, 'ARM', 645454)"
+				+ "(2021-06-12, 1, 'ARM', 534534)");
+		conn.createStatement().execute("insert into tipoVacuna values "
+				+ "('ARM', 'Pfizer', '2021/05/23'),"
+				+ "('ARM', 'Astrazeneca', '2021/07/03')"
+				+ "('ARM', 'Pfizer', '2021/06/34')"
+				+ "('ARM', 'Astrazeneca', '2021/04/30')");
+		conn.createStatement().execute("insert into paciente values "
+				+ "(223456, 'Agustin', 'Hernando'),"
+				+ "(534534, 'Antonio', 'Fernandez')"
+				+ "(645454, 'Rocio', 'Calatrava')"
+				+ "(123656, 'Rosa', 'Torrenova')");
 //	!!	private static String url = "jdbc:derby:directory:myDB;create=true";
 	}
 
