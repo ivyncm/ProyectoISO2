bd47b1d031a385a41affdeb184d4251b0a289a85
ivyncm <Ivan.Cantalejo@alu.uclm.es>
2021-10-09 13:11:52 +0200

    Base de Datos creada

--- src/main/java/ISO2/PrISO2/persistencia/AgenteBD.java
+++ src/main/java/ISO2/PrISO2/persistencia/AgenteBD.java
@@ -13,59 +13,59 @@ public class AgenteBD {
 		conectarBD();
 	}
 
-	public void conectarBD() throws Exception {
+	public static void conectarBD() throws Exception {
 		Class.forName(driver);
 		conn = DriverManager.getConnection(url);
 	}
 
-	public void crearBD() throws Exception {
+	public static void crearBD() throws Exception {
 		Class.forName(driver);
 		Connection conn = DriverManager.getConnection(url);
 		conn.createStatement().execute(
-				"create table entrega(fecha date, cantidad int" //Clave foranea sin primaria??
-				+ "constraint fecha foreign key references lote(fecha))");
+				"create table tipoVacuna(nombre varchar(30), farmaceutica varchar(30), fechaAprobacion date, primary key(farmaceutica))");
 		conn.createStatement().execute(
-				"create table lote(id int, fecha date, cantidad int, farmaceutica varchar(30), primary key(id))");
+				"create table paciente(dni varchar(30), nombre varchar(30), apellidos varchar(30), region varchar(30), grupoPrioridad varchar(30), primary key(dni))");
 		conn.createStatement().execute(
-				"create table tipoVacuna(nombre varchar(30), farmaceutica varchar(30), fechaAprobacion varchar(30), primary key(nombre), "
-				+ "constraint farmaceutica foreign key references lote(farmaceutica))");
+				"create table lote(id int, fecha date, cantidad int, nombre_tipoVacuna varchar(30), primary key(id))");
+				//+ "constraint nombre_tipoVacuna foreign key references tipoVacuna(farmaceutica))");
 		conn.createStatement().execute(
-				"create table vacunacion(fecha date, isSegundaDosis int, nombre_tipovacuna varchar(30), dni_paciente varchar(30), "
-				+ "constraint nombre_tipovacuna foreign key references tipoVacuna(nombre)"
-				+ "constraint dni_paciente foreign key references paciente(dni))"); //Lío
+				"create table entrega(fecha date, cantidad int, idLote int, region varchar(30), grupoPrioridad varchar(30))"); //Clave foranea sin primaria??
+		//		+ "constraint idLote foreign key references lote(id))");
 		conn.createStatement().execute(
-				"create table paciente(dni varchar(30), nombre varchar(30), apellidos varchar(30), primary key(dni))");
+				"create table vacunacion(fecha date, isSegundaDosis boolean, nombre_tipovacuna varchar(30), dni_paciente varchar(30))");
+				//+ "constraint nombre_tipovacuna foreign key references tipoVacuna(farmaceutica), "
+				//+ "constraint dni_paciente foreign key references paciente(dni))"); //Lío
 		
-		
-		conn.createStatement().execute("insert into entrega values "
-				+ "(2021-05-23, 5),"
-				+ "(2021-07-03, 500)"
-				+ "(2021-09-15, 300)"
-				+ "(2021-08-23, 3000)");
-		conn.createStatement().execute("insert into lote values "
-				+ "(153456, 2021-05-24, 5, 'Astrazeneca'),"
-				+ "(485667, 2021-07-04, 500, 'Pfizer')"
-				+ "(194563, 2021-09-16, 300, 'Pfizer')"
-				+ "(477687, 2021-08-24, 3000, 'Moderna')");
-		conn.createStatement().execute("insert into vacunacion values "
-				+ "(2021-05-26, 0, 'ARM', 223456)"
-				+ "(2021-04-24, 1, 'ARM', 123656)"
-				+ "(2021-02-23, 1, 'ARM', 645454)"
-				+ "(2021-06-12, 1, 'ARM', 534534)");
+
 		conn.createStatement().execute("insert into tipoVacuna values "
-				+ "('ARM', 'Pfizer', '2021/05/23'),"
-				+ "('ARM', 'Astrazeneca', '2021/07/03')"
-				+ "('ARM', 'Pfizer', '2021/06/34')"
-				+ "('ARM', 'Astrazeneca', '2021/04/30')");
+				+ "('BNT162b2', 'Pfizer', '2021-05-23'),"
+				+ "('SPIKEVAX', 'Moderna', '2021-07-03'),"
+				+ "('VAXZEVRIA', 'Astrazeneca', '2021-04-30')");
 		conn.createStatement().execute("insert into paciente values "
-				+ "(223456, 'Agustin', 'Hernando'),"
-				+ "(534534, 'Antonio', 'Fernandez')"
-				+ "(645454, 'Rocio', 'Calatrava')"
-				+ "(123656, 'Rosa', 'Torrenova')");
+				+ "('223456', 'Agustin', 'Hernando', 'Asturias', '>80'),"
+				+ "('534534', 'Antonio', 'Fernandez', 'Murcia', '60-70'),"
+				+ "('645454', 'Rocio', 'Calatrava', 'Andalucia', '>80'),"
+				+ "('123656', 'Rosa', 'Torrenova', 'CLM', '50-60')");
+		conn.createStatement().execute("insert into lote values "
+				+ "(153456, '2021-05-24', 5, 'Astrazeneca'),"
+				+ "(485667, '2021-07-04', 500, 'Pfizer'),"
+				+ "(194563, '2021-09-16', 300, 'Pfizer'),"
+				+ "(477687, '2021-08-24', 3000, 'Moderna')");
+		conn.createStatement().execute("insert into entrega values "
+				+ "('2021-05-23', 5, 153456, 'Asturias', '>80'),"
+				+ "('2021-07-03', 500, 485667, 'CLM', '50-60'),"
+				+ "('2021-09-15', 300, 194563, 'Murcia', '60-70'),"
+				+ "('2021-08-23', 3000, 477687, 'Andalucia', '>80')");
+		conn.createStatement().execute("insert into vacunacion values "
+				+ "('2021-05-26', False, 'Astrazeneca', '223456'),"
+				+ "('2021-04-24', True, 'Pfizer', '123656'),"
+				+ "('2021-02-23', True, 'Moderna', '645454'),"
+				+ "('2021-06-12', True, 'Pfizer', '534534')");
+			
 //	!!	private static String url = "jdbc:derby:directory:myDB;create=true";
 	}
 
-	public void desconectarBD() throws Exception {
+	public static void desconectarBD() throws Exception {
 		conn.close();
 	}
 
@@ -74,7 +74,7 @@ public class AgenteBD {
 	 * @param sql
 	 */
 
-	public ResultSet select(String sql) throws Exception {
+	public static ResultSet select(String sql) throws Exception {
 		conectarBD();
 		Statement stat = conn.createStatement();
 		ResultSet res = stat.executeQuery(sql);
@@ -120,11 +120,6 @@ public class AgenteBD {
 		stat.close();
 		desconectarBD();
 		return res;
-	}
-	
-	public void main (String args[]) throws Exception {
-		crearBD();
-		
 	}
 
 }
