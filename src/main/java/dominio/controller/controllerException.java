5b32a367133fb18881536397e32d2e512d1097dd
ivyncm <Ivan.Cantalejo@alu.uclm.es>
2021-11-30 19:19:35 +0100

    Eliminación TODOs

--- src/main/java/dominio/controller/controllerException.java
+++ src/main/java/dominio/controller/controllerException.java
@@ -4,17 +4,14 @@ public class controllerException extends Exception{
 
 		public controllerException(String message, Throwable cause) {
 			super(message, cause);
-			// TODO Auto-generated constructor stub
 		}
 
 		public controllerException(String message) {
 			super(message);
-			// TODO Auto-generated constructor stub
 		}
 
 		public controllerException(Throwable cause) {
 			super(cause);
-			// TODO Auto-generated constructor stub
 		}
 
 }
