4397d244b51fcf0dc19a756145697329f35cf7e3
ivyncm <Ivan.Cantalejo@alu.uclm.es>
2021-12-01 12:21:50 +0100

    Agregados system.out estadísticas regionales

--- src/main/java/presentacion/PantallaConsultaEstadisticas.java
+++ src/main/java/presentacion/PantallaConsultaEstadisticas.java
@@ -82,16 +82,24 @@ public class PantallaConsultaEstadisticas {
 					+ "3 - Volver");
 
 			op1 = teclado.nextInt();
+			List<Integer> totalVacunados;
+			List<Double> porcentajeRegion;
+			
 			switch (op1) {
 			case 1:
 				System.out.println("\n- " + region);
-				gestor.consultarTotalVacunadosPorRegion(region);
-
+				totalVacunados = gestor.consultarTotalVacunadosPorRegion(region);
+				System.out.println("Una dosis: " + (totalVacunados.get(0) - totalVacunados.get(1)) 
+						+"\nPauta completa: " + totalVacunados.get(0));
 				break;
 			case 2:
 				System.out.println("\n- " + region);
-				gestor.consultarPorcentajeVacunadosSobreRecibidasEnRegion(region);
-
+				porcentajeRegion = gestor.consultarPorcentajeVacunadosSobreRecibidasEnRegion(region);
+				if (porcentajeRegion.get(1) == 0) {
+					System.out.println("Porcentaje vacunados: 0,000%");
+				} else {
+					System.out.printf("Porcentaje vacunados: %.3f%n", (porcentajeRegion.get(0) / porcentajeRegion.get(1)) * 100);
+				}
 				break;
 			case 3:
 				PantallaGestionSistemaRegionalSalud.menu(region);
