89137e0c9b1638154b1a9158e51d9b31e6f537f2
ivyncm <Ivan.Cantalejo@alu.uclm.es>
2021-12-04 16:01:15 +0100

    Métodos dar de alta lote y calcular reparto

--- src/main/java/presentacion/PantallaGestionSistemaSaludNacional.java
+++ src/main/java/presentacion/PantallaGestionSistemaSaludNacional.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 
 import dominio.entitymodel.EntregaVacunas;
 import dominio.entitymodel.LoteVacunas;
+import persistencia.DAOException;
 
 import java.time.LocalDate;
 
+import dominio.controller.ControllerException;
 import dominio.controller.GestorRepartoVacunas;
 
 public class PantallaGestionSistemaSaludNacional {
@@ -19,8 +21,6 @@ public class PantallaGestionSistemaSaludNacional {
 		String noValido = "Opción no válida\n";
 		Scanner teclado = new Scanner(System.in);
 		int op1 = 0;
-		int op2 = 0;
-		String tipo = null;
 		System.out.println("Bienvenido al Sistema de Salud Nacional\n"
 				+ "seleccione una categoria de su interes.");
 		
@@ -34,76 +34,10 @@ public class PantallaGestionSistemaSaludNacional {
 			GestorRepartoVacunas gestor = new GestorRepartoVacunas();
 			switch (op1) {
 			case 1:
-				do {
-					System.out.println("Introduce el tipo de vacuna:\n"
-							+ "1 - Pfizer\n"
-							+ "2 - Moderna\n"
-							+ "3 - Astrazeneca");
-					op2 = teclado.nextInt();
-					switch (op2) {
-					case 1:
-						tipo = "Pfizer";
-						break;
-					case 2:
-						tipo = "Moderna";
-						break;
-					case 3:
-						tipo = "Astrazeneca";
-						break;
-					default:
-						System.out.println(noValido);
-						break;
-					}
-				} while (op2 < 1 || op2 > 3);
-
-				System.out.println("Introduce la cantidad:");
-				int cantidad = teclado.nextInt();
-				
-
-				LocalDate fecha = LocalDate.now();
-
-				gestor.altaNuevoLoteVacunas(fecha, tipo, cantidad);
+				altaNuevoLote(gestor);
 				break;
 			case 2:
-				LoteVacunas lote = null;
-				List<LoteVacunas> lotes = gestor.imprimirLotes();
-				System.out.println("Seleccionar lote a repartir (introducir número)");
-				for(int i = 0; i<lotes.size(); i++){
-					System.out.println("Lote " + (i+1) + lotes.get(i).toString());
-				}
-				do {
-					op2 = teclado.nextInt();
-					if(op2>0 && op2<lotes.size()+1) {
-						lote = lotes.get(op2-1);
-						System.out.println("Lote seleccionado" + lote.toString());
-					}
-				} while (op2<1 || op2>lotes.size());
-				String prioridad = null;
-				do {
-					System.out.println("Introduce el grupo de prioridad:\n"
-							+ "1 - Joven\n"
-							+ "2 - Adulto\n"
-							+ "3 - Anciano");
-					op2 = teclado.nextInt();
-					switch (op2) {
-					case 1:
-						prioridad = "Joven";
-						break;
-					case 2:
-						prioridad = "Adulto";
-						break;
-					case 3:
-						prioridad = "Anciano";
-						break;
-					default:
-						System.out.println(noValido);
-						break;
-					}
-				} while (op2 < 1 || op2 > 3);
-				List<EntregaVacunas> entregas = gestor.calcularEntregasRegion(lote, prioridad);
-				for(int i = 0; i<entregas.size(); i++){
-					System.out.println("Entrega " + (i+1) + entregas.get(i).toString());
-				}
+				calcularReparto(gestor);
 				break;
 			case 3:
 				PantallaConsultaEstadisticas.estadNacional();
@@ -117,5 +51,86 @@ public class PantallaGestionSistemaSaludNacional {
 			}
 		} while (op1 < 1 || op1 > 4);
 		teclado.close();
+	}
+	
+	public static void altaNuevoLote(GestorRepartoVacunas gestor) throws ControllerException {
+		Scanner teclado = new Scanner(System.in);
+		String noValido = "Opción no válida\n";
+		String tipo = null;
+		int op2;
+		do {
+			System.out.println("Introduce el tipo de vacuna:\n"
+					+ "1 - Pfizer\n"
+					+ "2 - Moderna\n"
+					+ "3 - Astrazeneca");
+			op2 = teclado.nextInt();
+			switch (op2) {
+			case 1:
+				tipo = "Pfizer";
+				break;
+			case 2:
+				tipo = "Moderna";
+				break;
+			case 3:
+				tipo = "Astrazeneca";
+				break;
+			default:
+				System.out.println(noValido);
+				break;
+			}
+		} while (op2 < 1 || op2 > 3);
+
+		System.out.println("Introduce la cantidad:");
+		int cantidad = teclado.nextInt();
+		
+
+		LocalDate fecha = LocalDate.now();
+
+		gestor.altaNuevoLoteVacunas(fecha, tipo, cantidad);
+	}
+	
+	public static void calcularReparto(GestorRepartoVacunas gestor) throws DAOException, ControllerException {
+		Scanner teclado = new Scanner(System.in);
+		String noValido = "Opción no válida\n";
+		LoteVacunas lote = null;
+		int op2;
+		List<LoteVacunas> lotes = gestor.imprimirLotes();
+		System.out.println("Seleccionar lote a repartir (introducir número)");
+		for(int i = 0; i<lotes.size(); i++){
+			System.out.println("Lote " + (i+1) + lotes.get(i).toString());
+		}
+		do {
+			op2 = teclado.nextInt();
+			if(op2>0 && op2<lotes.size()+1) {
+				lote = lotes.get(op2-1);
+				System.out.println("Lote seleccionado" + lote.toString());
+			}
+		} while (op2<1 || op2>lotes.size());
+		String prioridad = null;
+		do {
+			System.out.println("Introduce el grupo de prioridad:\n"
+					+ "1 - Joven\n"
+					+ "2 - Adulto\n"
+					+ "3 - Anciano");
+			op2 = teclado.nextInt();
+			switch (op2) {
+			case 1:
+				prioridad = "Joven";
+				break;
+			case 2:
+				prioridad = "Adulto";
+				break;
+			case 3:
+				prioridad = "Anciano";
+				break;
+			default:
+				System.out.println(noValido);
+				break;
+			}
+		} while (op2 < 1 || op2 > 3);
+		List<EntregaVacunas> entregas = gestor.calcularEntregasRegion(lote, prioridad);
+		for(int i = 0; i<entregas.size(); i++){
+			System.out.println("Entrega " + (i+1) + entregas.get(i).toString());
+		}
 	}
 }
