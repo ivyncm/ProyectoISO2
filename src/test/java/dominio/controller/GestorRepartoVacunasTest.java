386cfa4900648525588103ba42a72b7de61be2d4
ivyncm <Ivan.Cantalejo@alu.uclm.es>
2021-12-15 12:20:11 +0100

    Se actualizan tests GestorRepartoVacunas

--- src/test/java/dominio/controller/GestorRepartoVacunasTest.java
+++ src/test/java/dominio/controller/GestorRepartoVacunasTest.java
@@ -1,7 +1,5 @@
 package dominio.controller;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,32 +13,33 @@ import persistencia.DAOException;
 public class GestorRepartoVacunasTest {
 
 	@Test
-	public void testAltaNuevoLoteVacunas() throws ControllerException {
+	public void testAltaNuevoLoteVacunas() throws ControllerException, DAOException {
 		LocalDate fecha = LocalDate.now();
 		String tipo = "Pfizer";
 		int cantidad = 100;
 		GestorRepartoVacunas reparto = new GestorRepartoVacunas();
-		reparto.altaNuevoLoteVacunas(fecha, tipo, cantidad);
+		LoteVacunas expected = new LoteVacunas(fecha, cantidad, tipo);
+		reparto.altaNuevoLoteVacunas(expected);
 		
-		String expected = "";
-	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    PrintStream printStream = new PrintStream(baos);
-	    System.setOut(printStream);
-	    String[] lines = baos.toString().split(System.lineSeparator());
-	    String actual = lines[lines.length-1];
-		Assert.assertEquals(expected, actual);
+		
+		LoteVacunas actual = expected.getLoteVacunasDao().get(expected.getId());
+		
+		
+		Assert.assertTrue(expected.equals(actual));
 	}
 
 	@Test
 	public void testCalcularEntregasRegionMayor200() throws DAOException, ControllerException {
 		String id = "asds51";
 		LocalDate fecha = LocalDate.now(); 
-		int cantidad = 200; 
+		int cantidad = 300; 
 		String farmaceutica = "Pfizer";
 		LoteVacunas lote = new LoteVacunas(id, fecha, cantidad, farmaceutica);
 		GestorRepartoVacunas reparto = new GestorRepartoVacunas();
 		List<EntregaVacunas> entregas = reparto.calcularEntregasRegion(lote, "Joven");
-		Assert.assertNotEquals(entregas, null);
+		
+		
+		Assert.assertTrue(entregas.size() == 19);
 	}
 	@Test
 	public void testCalcularEntregasRegionMenor200() throws DAOException, ControllerException {
@@ -51,7 +50,8 @@ public class GestorRepartoVacunasTest {
 		LoteVacunas lote = new LoteVacunas(id, fecha, cantidad, farmaceutica);
 		GestorRepartoVacunas reparto = new GestorRepartoVacunas();
 		List<EntregaVacunas> entregas = reparto.calcularEntregasRegion(lote, "Joven");
-		Assert.assertNotEquals(entregas, null);
+		
+		Assert.assertTrue(entregas.size() == 1);
 	}
 
 	@Test
